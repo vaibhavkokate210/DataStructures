88ba623b232b6c8e2114ef924e8cf12dfb80e911
vaibhavkokate210 <vaibhavkokate210@gmail.com>
2021-08-25 10:01:47 +0530

    [Vaibhav] added initial commit

---
+++ src/com/bridgelabz/linkedlist/LinkedList.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.linkedlist;
+
+public class LinkedList 
+{
+	public static void main(String[] args)
+	{
+		
+		
+	}
+}
---
+++ src/com/bridgelabz/linkedlist/MyLinkedList.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.linkedlist;
+
+public class MyLinkedList<K>
+{
+	private INode head;
+	private INode tail;
+	
+	public MyLinkedList() {
+		super();
+		this.head = null;
+	}
+	
+	
+}
